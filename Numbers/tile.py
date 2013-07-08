353b01749279c8336a7281860e6f495128da041d
Ross Freiman <rossfreiman2013@u.northwestern.edu>
2013-07-08 17:10:18 -0500

    added tiling function

---
+++ Numbers/tile.py
@@ -0,0 +1,20 @@
+#Calculate the total cost of tile it would take to cover a floor plan of width 
+#and height, using a cost entered by the user.
+
+def tile(cost, width, height):
+	return cost*width*height
+
+
+def test():
+	assert tile(10,10,10) == 1000
+	assert tile(15,10,10) == 1500
+	assert tile(0,10,10) == 0
+
+
+if __name__ == '__main__':
+	test()
+	cost = input("Enter cost of a sqft. tile: ")
+	width = input("Enter width of floor plan: ")
+	height = input("Enter height of floor plan: ")
+	
+	print "The cost of the tiling is $%.2f" %(tile(cost, width, height))
